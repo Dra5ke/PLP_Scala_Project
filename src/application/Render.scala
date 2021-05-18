17110a52feabadb07e15d7df899e3cbfce4c8a05
bblana61@gmail.com <bblana61@gmail.com>
2021-05-19 00:28:05 +0200

    timeToBed

--- src/application/Render.scala
+++ src/application/Render.scala
@@ -1,28 +1,43 @@
 package application
 
 import javafx.scene.chart.XYChart
+import javafx.scene.chart.XYChart.Series
 
 class Render {
   // (LINE  (7 8) (12 12))
   // (CIRCLE (7 8) 5)
   // (RECTANGLE (12 14) (24 44))
+  var boundingBox: List[Int] = List(0,1,2,3) //x0,y0,x1,y1
+
   def getShape(command: String): XYChart.Series[Number, Number] = {
-    var shape = command.filter(!_.isDigit).replaceAll("\\W", "")
-    var numberString = command.replaceAll("[^\\d ]", "");
+    val shape = command.filter(!_.isDigit).replaceAll("\\W", "")
+    val numberString = command.replaceAll("[^\\d ]", "");
 
 
     shape match {
       case "LINE" => {
         val list = numberString.split("\\D+").filter(_.nonEmpty).toList.map(_.toInt)
-        return drawLine(list(0), list(1), list(2), list(3))
+        return cleanBoundPixels(drawLine(list(0), list(1), list(2), list(3)))
       }
       case "CIRCLE" => {
         val list = numberString.split("\\D+").filter(_.nonEmpty).toList.map(_.toInt)
-        return drawCircle(list(0), list(1), list(2))
+        return cleanBoundPixels(drawCircle(list(0), list(1), list(2)))
       }
       case "RECTANGLE" => {
         val list = numberString.split("\\D+").filter(_.nonEmpty).toList.map(_.toInt)
-        return drawRectangle(list(0), list(1), list(2), list(3))
+        return cleanBoundPixels(drawRectangle(list(0), list(1), list(2), list(3)))
+      }
+      case "BOUNDINGBOX" => {
+         boundingBox = numberString.split("\\D+").filter(_.nonEmpty).toList.map(_.toInt)
+        return drawRectangle(boundingBox(0), boundingBox(1), boundingBox(2), boundingBox(3))
+      }
+      case "BARCHART" => {
+        val list = numberString.split("\\D+").filter(_.nonEmpty).toList.map(_.toInt)
+        return cleanBoundPixels(drawBarChart(list))
+      }
+      case "PIECHART" => {
+        val list = numberString.split("\\D+").filter(_.nonEmpty).toList.map(_.toInt)
+        return cleanBoundPixels(drawPieChart(list))
       }
     }
   }
@@ -43,10 +58,12 @@ class Render {
         val res = (x, y)
         val e2 = err;
         if (e2 > -dx) {
-          err -= dy; x += sx
+          err -= dy;
+          x += sx
         }
         if (e2 < dy) {
-          err += dx; y += sy
+          err += dx;
+          y += sy
         }
         res;
       }
@@ -57,7 +74,6 @@ class Render {
     val pixels = new XYChart.Series[Number, Number]
 
     for ((x, y) <- it) {
-      println(x + "," + y)
       pixels.getData().add(new XYChart.Data(x, y))
     }
 
@@ -66,38 +82,36 @@ class Render {
 
   def drawCircle(x0: Int, y0: Int, radius: Int): XYChart.Series[Number, Number] = {
     val pixels = new XYChart.Series[Number, Number]
-    var f=1-radius
-    var ddF_x=1
-    var ddF_y= -2*radius
-    var x=0
-    var y=radius
+    var f = 1 - radius
+    var ddF_x = 1
+    var ddF_y = -2 * radius
+    var x = 0
+    var y = radius
 
-    pixels.getData().add(new XYChart.Data(x0, y0+radius))
-    pixels.getData().add(new XYChart.Data(x0, y0-radius))
-    pixels.getData().add(new XYChart.Data(x0+radius, y0))
-    pixels.getData().add(new XYChart.Data(x0-radius, y0))
+    pixels.getData().add(new XYChart.Data(x0, y0 + radius))
+    pixels.getData().add(new XYChart.Data(x0, y0 - radius))
+    pixels.getData().add(new XYChart.Data(x0 + radius, y0))
+    pixels.getData().add(new XYChart.Data(x0 - radius, y0))
 
-    while(x < y)
-    {
-      if(f >= 0)
-      {
-        y-=1
-        ddF_y+=2
-        f+=ddF_y
+    while (x < y) {
+      if (f >= 0) {
+        y -= 1
+        ddF_y += 2
+        f += ddF_y
       }
-      x+=1
-      ddF_x+=2
-      f+=ddF_x
+      x += 1
+      ddF_x += 2
+      f += ddF_x
 
 
-      pixels.getData().add(new XYChart.Data(x0+x, y0+y))
-      pixels.getData().add(new XYChart.Data(x0-x, y0+y))
-      pixels.getData().add(new XYChart.Data(x0+x, y0-y))
-      pixels.getData().add(new XYChart.Data(x0-x, y0-y))
-      pixels.getData().add(new XYChart.Data(x0+y, y0+x))
-      pixels.getData().add(new XYChart.Data(x0-y, y0+x))
-      pixels.getData().add(new XYChart.Data(x0+y, y0-x))
-      pixels.getData().add(new XYChart.Data(x0-y, y0-x))
+      pixels.getData().add(new XYChart.Data(x0 + x, y0 + y))
+      pixels.getData().add(new XYChart.Data(x0 - x, y0 + y))
+      pixels.getData().add(new XYChart.Data(x0 + x, y0 - y))
+      pixels.getData().add(new XYChart.Data(x0 - x, y0 - y))
+      pixels.getData().add(new XYChart.Data(x0 + y, y0 + x))
+      pixels.getData().add(new XYChart.Data(x0 - y, y0 + x))
+      pixels.getData().add(new XYChart.Data(x0 + y, y0 - x))
+      pixels.getData().add(new XYChart.Data(x0 - y, y0 - x))
     }
 
     return pixels
@@ -107,23 +121,110 @@ class Render {
     val pixels = new XYChart.Series[Number, Number]
 
 
-    val verticalLeft = drawLine(x0,y0, x0, y1)
+    val verticalLeft = drawLine(x0, y0, x0, y1)
     verticalLeft.getData().forEach(p => {
       pixels.getData().add(p)
     })
 
-    val verticalRight = drawLine(x1,y0, x1, y1)
+    val verticalRight = drawLine(x1, y0, x1, y1)
     verticalRight.getData().forEach(p => {
       pixels.getData().add(p)
     })
 
-    val horizontalTop = drawLine(x0,y1, x1, y1)
+    val horizontalTop = drawLine(x0, y1, x1, y1)
     horizontalTop.getData().forEach(p => {
       pixels.getData().add(p)
     })
 
-    val horizontalBottom = drawLine(x0,y0, x1, y0)
+    val horizontalBottom = drawLine(x0, y0, x1, y0)
     horizontalBottom.getData().forEach(p => {
+      pixels.getData().add(p)
+    })
+
+    return pixels
+  }
+
+  def cleanBoundPixels(series: XYChart.Series[Number, Number]): XYChart.Series[Number, Number] = {
+
+    var x0 = boundingBox(0)
+    var y0 = boundingBox(1)
+    var x1 = boundingBox(2)
+    var y1 = boundingBox(3)
+
+    var cleanSeries = new Series[Number, Number]()
+
+    series.getData().forEach(p => {
+      var p_x = p.getXValue().doubleValue()
+      var p_y = p.getYValue().doubleValue()
+
+      if ((p_x > x0 && p_x < x1) && (p_y > y0 && p_y < y1)) {
+        cleanSeries.getData().add(p)
+      }
+    })
+
+    return cleanSeries
+  }
+
+  def drawBarChart(data : List[Int]): XYChart.Series[Number, Number] = {
+    val pixels = new XYChart.Series[Number, Number]
+
+    val boundingBoxHeight = boundingBox(3) -  boundingBox(1)
+    //boundingBox() x0,y0,x1,y1
+    val barHeight = boundingBoxHeight / data.length
+
+    var tempRectangle = new XYChart.Series[Number, Number]
+
+    var count:Int = 0
+
+    for( x <- data ){
+      println(count)
+      println("x0: "+ boundingBox(0) + "y0: " + (boundingBox(1) + barHeight*count) + "x1: "+(boundingBox(0) + x)  + "y1: "+ (barHeight*(count+1)))
+      tempRectangle = drawRectangle(boundingBox(0),boundingBox(1) + barHeight*count, boundingBox(0) + x, barHeight*(count+1))
+
+      tempRectangle.getData().forEach(p => {
+        pixels.getData().add(p)
+      })
+      count += 1
+    }
+
+    return pixels
+  }
+
+  def drawPieChart(data : List[Int]): XYChart.Series[Number, Number] = {
+    val pixels = new XYChart.Series[Number, Number]
+    var tempRectangle = new XYChart.Series[Number, Number]
+
+    var width = boundingBox(2) - boundingBox(0)
+    var height = boundingBox(3) - boundingBox(1)
+
+    var shortSide : Int = 0
+
+    if( width < height){
+        shortSide = width
+    } else{
+      shortSide = height
+    }
+
+    var centerX = (boundingBox(0)+ boundingBox(2)/2)
+    var centerY = (boundingBox(1)+ boundingBox(3)/2)
+
+     drawCircle((boundingBox(0)+ boundingBox(2)/2),(boundingBox(1)+ boundingBox(3)/2), shortSide/2)
+
+    var count:Int = 0
+
+    for( x <- data ){
+      println(count)
+     // println("x0: "+ boundingBox(0) + "y0: " + (boundingBox(1) + barHeight*count) + "x1: "+(boundingBox(0) + x)  + "y1: "+ (barHeight*(count+1)))
+     // tempRectangle = drawRectangle(boundingBox(0),boundingBox(1) + barHeight*count, boundingBox(0) + x, barHeight*(count+1))
+
+      tempRectangle.getData().forEach(p => {
+        pixels.getData().add(p)
+      })
+      count += 1
+    }
+
+
+    tempRectangle.getData().forEach(p => {
       pixels.getData().add(p)
     })
 
